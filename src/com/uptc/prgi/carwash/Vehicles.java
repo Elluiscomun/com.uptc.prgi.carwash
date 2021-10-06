5105cfcc8d0455232e9ca33ee0e20a551aa04c4f
Elluiscomun <luismiguelsantiago@outlook.es>
2021-10-06 17:38:37 -0500

    CarWash3

---
+++ src/com/uptc/prgi/carwash/Vehicles.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.uptc.prgi.carwash;
+
+/**
+ *
+ * @author Luis Miguel Santiago 202023610
+ * @date 6/10/2021  
+ */
+public class Vehicles {
+
+}
