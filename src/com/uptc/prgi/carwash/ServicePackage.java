0e8ca40a08d977c93e35c1d5d3a9dba04be17565
57321 <57321@LAPTOP-STRANGE24>
2021-10-07 10:11:34 -0500

    First Version

--- src/com/uptc/prgi/carwash/ServicePackage.java
+++ src/com/uptc/prgi/carwash/ServicePackage.java
@@ -10,5 +10,22 @@ package com.uptc.prgi.carwash;
  * @author luism
  */
 public enum ServicePackage {
+    INTERIOR_WASH("Lavado Interior", 8000), EXTERIOR_WASH("Lavado Exterior", 12000), ENGINE_WASH("Lavado de Motor", 10000) ;
     
+    private final String typeOfService;
+    private final int priceOfService;
+    
+    
+    private ServicePackage(String typeOfService, int priceOfService){
+        this.typeOfService=typeOfService;
+        this.priceOfService=priceOfService;
+    }
+    
+    public String getTypeOfService(){
+        return this.typeOfService;
+    }
+    
+    public int getPriceOfService(){
+        return this.priceOfService;
+    }
 }
