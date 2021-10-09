e01d95904545ee52c73ab74ec2680e7e1fa73598
NikoBellic-18 <92051929+NikoBellic-18@users.noreply.github.com>
2021-10-08 19:37:02 -0500

    1.1N

--- src/com/uptc/prgi/carwash/SalesReport.java
+++ src/com/uptc/prgi/carwash/SalesReport.java
@@ -5,6 +5,7 @@
  */
 
 package com.uptc.prgi.carwash;
+import java.util.Comparator;
 
 /**
  *
@@ -12,8 +13,32 @@ package com.uptc.prgi.carwash;
  * @date 6/10/2021  
  */
 public class SalesReport<T> {
-        
+
     private T[] datas;
     
+    public SalesReport(T[] datas) {
+        this.datas = datas;
+    }
+    
+    private void swap(int i, int j){
+        T aux = this.datas[j];
+        this.datas[j]=this.datas[i];    
+        this.datas[i]=aux;
+    }
+    
+    public SalesReport<T> bubbleSort(Comparator<T> comparator) {
+        SalesReport<T> result = new SalesReport(datas);
+        boolean change = true;
+        while (change) {
+            change = false;
+            for (int i = 0; i < datas.length - 1; i++) {
+                if (comparator.compare(this.datas[i], result.datas[i + 1]) > 0) {
+                    result.swap(i, i + 1);
+                    change = true;
+                }
+            }
+        }
+        return result;
+    }
     
 }
