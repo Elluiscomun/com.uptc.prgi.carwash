6bc71ea393e960eae35ce25c103046d0e74da0ba
57321 <57321@LAPTOP-STRANGE24>
2021-10-10 20:30:25 -0500

    2.8 Ed

--- test/Test/TestMain.java
+++ test/Test/TestMain.java
@@ -13,6 +13,7 @@ import static com.uptc.prgi.carwash.ServicePackage.ENGINE_WASH;
 import static com.uptc.prgi.carwash.ServicePackage.EXTERIOR_WASH;
 import static com.uptc.prgi.carwash.ServicePackage.INTERIOR_WASH;
 import com.uptc.prgi.carwash.TypeVehicle;
+import static com.uptc.prgi.carwash.TypeVehicle.PARTICULAR;
 import static com.uptc.prgi.carwash.TypeVehicle.PUBLIC;
 import com.uptc.prgi.carwash.Users;
 import com.uptc.prgi.carwash.Vehicles;
@@ -120,19 +121,19 @@ public class TestMain {
                 "321598310099", new Vehicles("NPL-329", PUBLIC, new ServicePackage[]{INTERIOR_WASH})));
         
         Sale sale3 = new Sale(new Users("Alex Moreno", 
-                "3190031043", new Vehicles("KKD-100", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH, ENGINE_WASH})));
+                "3190031043", new Vehicles("KKD-100", PARTICULAR, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH, ENGINE_WASH})));
         
         Sale sale4 = new Sale(new Users("Maria Gallejo", 
                 "3118036108", new Vehicles("GAC-003", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH, ENGINE_WASH})));
         
         Sale sale5 = new Sale(new Users("Andres Gutierrez", 
-                "3134597958", new Vehicles("HIO-978", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH, ENGINE_WASH})));
+                "3134597958", new Vehicles("HIO-978", PARTICULAR, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH, ENGINE_WASH})));
         
         Sale sale6 = new Sale(new Users("Danna Herandez", 
-                "3203767200", new Vehicles("OUH-666", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH})));
+                "3203767200", new Vehicles("OUH-676", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH})));
         
         Sale sale7 = new Sale(new Users("Miguel Castellanos", 
-                "3501268922", new Vehicles("WEE-086", PUBLIC, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH})));
+                "3501268922", new Vehicles("WEE-086", PARTICULAR, new ServicePackage[]{INTERIOR_WASH, EXTERIOR_WASH})));
         
         Sale sale8 = new Sale(new Users("Emerson Castro", 
                 "3518935478", new Vehicles("KYC-862", PUBLIC, new ServicePackage[]{INTERIOR_WASH})));
@@ -141,9 +142,9 @@ public class TestMain {
                 "3109453217", new Vehicles("DLE-999", PUBLIC, new ServicePackage[]{INTERIOR_WASH})));
         
         Sale sale10 = new Sale(new Users("Edwin Vargas", 
-                "3158963145", new Vehicles("MSX-409", PUBLIC, new ServicePackage[]{})));
-       
-        salesReport2 = new SalesReport(new Sale[]{sale1,sale2,sale3,sale4,sale5,sale7,sale8,sale9,sale10});
+                "3158963145", new Vehicles("MSX-409", PUBLIC, new ServicePackage[]{ENGINE_WASH})));
+        
+        salesReport2 = new SalesReport(new Sale[]{sale1,sale2,sale3,sale4,sale5,sale6,sale7,sale8,sale9,sale10});
     }
     
     public ServicePackage[] createServicePackageArray(int numberlength){
