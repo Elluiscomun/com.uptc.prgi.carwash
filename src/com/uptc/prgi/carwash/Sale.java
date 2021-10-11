e98e9b0899dd7cf25cc5c4485d4b22770ba81b93
Elluiscomun <luismiguelsantiago@outlook.es>
2021-10-10 21:45:58 -0500

    3.9L

--- src/com/uptc/prgi/carwash/Sale.java
+++ src/com/uptc/prgi/carwash/Sale.java
@@ -51,7 +51,7 @@ public class Sale{
     /**
      * Determinar si el usuario aplica a la promocion/descuento del valor total a pagar si adquiere 
      * los tres servicios que ofrece el lavadero de autos "Car Wash"...
-     * @return boolean True-->Si aplica a la promocion/descuento...
+     * @return boolean
      */
     public boolean determinePromotion(){
         return user.getVehicles().getServicePackage().length==3;
@@ -61,7 +61,7 @@ public class Sale{
     /**
      * Determinar que si el vehiculo es de tipo PÚBLICO, por lo que si lo anterior resulta 
      * ser verdadero el usuario aplica para recibir un ambientador de obsequio...
-     * @return boolean-->Si aplica al obsequio del ambientador...
+     * @return 
      */
     public boolean determineAirFresher(){
         return user.getVehicles().getTypeVehicle() == PUBLIC;
@@ -87,7 +87,7 @@ public class Sale{
     
     @Override
     public String toString() {
-        return "Venta{" + "Usuario : " + user + ", date=" + date + ", value=" + value + '}';
+        return "Venta{" + "Usuario : " + user + "\n, Fecha=" + date + ", Valor=" + value + '}';
     }
     
 }
